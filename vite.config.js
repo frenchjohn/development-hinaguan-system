b6abe757a92b94159c9a3dd34b42f44a7f5e2432
frenchjohn <famador.frenchjohn123@gmail.com>
2026-09-22 19:46:41 +0800

    fix: add admin_payment.js to Vite input - fixes 500 on admin payment page in production

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ export default defineConfig({
                 'resources/js/admin_js/admin_usermanagement.js',
                 'resources/js/admin_js/admin_feedback.js',
                 'resources/js/admin_js/admin_announcement.js',
+                'resources/js/admin_js/admin_payment.js',
 
                 // Guest feedback page
                 'resources/css/feedback.css',
